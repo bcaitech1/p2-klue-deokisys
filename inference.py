7929574320925f8ba2b156439ac6a3f3ca48bc2c
junbae <sjb378@me.com>
2021-04-21 10:15:19 +0000

    add kykim model use
    e1,e2 token add
    lr5e-5,epoch4,batch8

--- inference.py
+++ inference.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, BertConfig, BertTokenizer
+from transformers import XLMRobertaForSequenceClassification, XLMRobertaTokenizer, AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, BertConfig, BertTokenizer
 from torch.utils.data import DataLoader
 from load_data import *
 import pandas as pd
@@ -7,67 +7,77 @@ import pickle as pickle
 import numpy as np
 import argparse
 
-def inference(model, tokenized_sent, device):
-  dataloader = DataLoader(tokenized_sent, batch_size=40, shuffle=False)
-  model.eval()
-  output_pred = []
-  
-  for i, data in enumerate(dataloader):
-    with torch.no_grad():
-      outputs = model(
-          input_ids=data['input_ids'].to(device),
-          attention_mask=data['attention_mask'].to(device),
-          token_type_ids=data['token_type_ids'].to(device)
-          )
-    logits = outputs[0]
-    logits = logits.detach().cpu().numpy()
-    result = np.argmax(logits, axis=-1)
 
-    output_pred.append(result)
-  
-  return np.array(output_pred).flatten()
+def inference(model, tokenized_sent, device):
+    dataloader = DataLoader(tokenized_sent, batch_size=40, shuffle=False)
+    model.eval()
+    output_pred = []
+
+    for i, data in enumerate(dataloader):
+        with torch.no_grad():
+            outputs = model(
+                input_ids=data['input_ids'].to(device),
+                attention_mask=data['attention_mask'].to(device),
+                token_type_ids=data['token_type_ids'].to(device)
+            )
+        logits = outputs[0]
+        logits = logits.detach().cpu().numpy()
+        result = np.argmax(logits, axis=-1)
+
+        output_pred.append(result)
+
+    return np.array(output_pred).flatten()
+
 
 def load_test_dataset(dataset_dir, tokenizer):
-  test_dataset = load_data(dataset_dir)
-  test_label = test_dataset['label'].values
-  # tokenizing dataset
-  tokenized_test = tokenized_dataset(test_dataset, tokenizer)
-  return tokenized_test, test_label
+    test_dataset = load_data(dataset_dir)
+    test_label = test_dataset['label'].values
+    # tokenizing dataset
+    tokenized_test = tokenized_dataset(test_dataset, tokenizer)
+    return tokenized_test, test_label
+
 
 def main(args):
-  """
-    주어진 dataset tsv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
-  """
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-  # load tokenizer
-  TOK_NAME = "bert-base-multilingual-cased"  
-  tokenizer = AutoTokenizer.from_pretrained(TOK_NAME)
+    """
+      주어진 dataset tsv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
+    """
+    # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
+    # load tokenizer
+    # "bert-base-multilingual-cased",kykim/bert-kor-base,xlm-roberta-large
+    TOK_NAME = "kykim/bert-kor-base"
+    tokenizer = AutoTokenizer.from_pretrained(
+        TOK_NAME)  # XLMRobertaTokenizer AutoTokenizer.from_pretrained(TOK_NAME)
 
-  # load my model
-  MODEL_NAME = args.model_dir # model dir.
-  model = BertForSequenceClassification.from_pretrained(args.model_dir)
-  model.parameters
-  model.to(device)
+    # load my model
+    MODEL_NAME = args.model_dir  # model dir.
+    # XLMRobertaForSequenceClassification BertForSequenceClassification.from_pretrained(args.model_dir)
+    model = BertForSequenceClassification.from_pretrained(args.model_dir)
 
-  # load test datset
-  test_dataset_dir = "/opt/ml/input/data/test/test.tsv"
-  test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
-  test_dataset = RE_Dataset(test_dataset ,test_label)
+    model.parameters
+    model.to(device)
 
-  # predict answer
-  pred_answer = inference(model, test_dataset, device)
-  # make csv file with predicted answer
-  # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+    # load test datset
+    test_dataset_dir = "/opt/ml/input/data/test/test.tsv"
+    test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
+    test_dataset = RE_Dataset(test_dataset, test_label)
 
-  output = pd.DataFrame(pred_answer, columns=['pred'])
-  output.to_csv('./prediction/submission.csv', index=False)
+    # predict answer
+    pred_answer = inference(model, test_dataset, device)
+    # make csv file with predicted answer
+    # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+
+    output = pd.DataFrame(pred_answer, columns=['pred'])
+    output.to_csv(
+        './prediction/submission_sep_ent_kim_e4_b8_lr5e-5_not_train_val.csv', index=False)
+
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  
-  # model dir
-  parser.add_argument('--model_dir', type=str, default="./results/checkpoint-500")
-  args = parser.parse_args()
-  print(args)
-  main(args)
-  
+    parser = argparse.ArgumentParser()
+
+    # model dir
+    parser.add_argument('--model_dir', type=str,
+                        default="./results/checkpoint-4500")
+    args = parser.parse_args()
+    print(args)
+    main(args)
--- train.py
+++ train.py
@@ -3,8 +3,24 @@ import os
 import pandas as pd
 import torch
 from sklearn.metrics import accuracy_score
-from transformers import AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, BertConfig
+from transformers import XLMRobertaConfig, XLMRobertaForSequenceClassification, XLMRobertaTokenizer, AutoModelForSequenceClassification, AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, BertConfig
 from load_data import *
+from sklearn.model_selection import train_test_split
+import numpy as np
+import wandb
+import random
+
+
+# 시드 고정
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
 
 # 평가를 위한 metrics function.
 
@@ -21,29 +37,50 @@ def compute_metrics(pred):
 
 def train():
     # load model and tokenizer
-    MODEL_NAME = "bert-base-multilingual-cased"
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    # "bert-base-multilingual-cased",kykim/bert-kor-base,roberta-large-mnli
+    MODEL_NAME = "kykim/bert-kor-base"
+    tokenizer = AutoTokenizer.from_pretrained(  # roberta기준
+        MODEL_NAME)  # XLMRobertaTokenizer AutoTokenizer.from_pretrained(MODEL_NAME)
+    # autoTokenizer로 자동으로 가져온다.
+
+    # 데이터셋 나누기
+    datas = pd.read_csv("/opt/ml/input/data/train/train.tsv",
+                        delimiter='\t', header=None)
+    train, val = train_test_split(datas, test_size=0.2, random_state=42)
+    # train = pd.DataFrame(data=train)
+    train.to_csv('/opt/ml/input/data/train/train_train.tsv',
+                 sep='\t', header=None, index=False)
+    val.to_csv('/opt/ml/input/data/train/train_val.tsv',
+               sep='\t', header=None, index=False)
 
     # load dataset
     train_dataset = load_data("/opt/ml/input/data/train/train.tsv")
-    #dev_dataset = load_data("./dataset/train/dev.tsv")
     train_label = train_dataset['label'].values
-    #dev_label = dev_dataset['label'].values
+
+    dev_dataset = load_data("/opt/ml/input/data/train/train.tsv")
+    dev_label = dev_dataset['label'].values
 
     # tokenizing dataset
     tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-    #tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+    tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
 
     # make dataset for pytorch.
     RE_train_dataset = RE_Dataset(tokenized_train, train_label)
-    #RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+    RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # setting model hyperparameter
-    bert_config = BertConfig.from_pretrained(MODEL_NAME)
+    bert_config = BertConfig.from_pretrained(
+        MODEL_NAME)  # XLMRobertaConfig,BertConfig.from_pretrained(MODEL_NAME)
+    # print(bert_config)
+
+    # bert_config.vocab_size = 42004  # 4개 추가 안됨
     bert_config.num_labels = 42
-    model = BertForSequenceClassification(bert_config)  # from_pretrained가져오기
+    model = AutoModelForSequenceClassification.from_pretrained(  # XLMRobertaForSequenceClassification AutoModelForSequenceClassification.from_pretrained(
+        MODEL_NAME, config=bert_config)
+    model.resize_token_embeddings(len(tokenizer))  # 새로운 토큰 추가로 임베딩 크기 조정
+    # model = BertForSequenceClassification(bert_config)  # from_pretrained가져오기
     model.parameters
     model.to(device)
 
@@ -55,25 +92,33 @@ def train():
         save_steps=500,                 # model saving step.
         num_train_epochs=4,              # total number of training epochs
         learning_rate=5e-5,               # learning_rate
-        per_device_train_batch_size=16,  # batch size per device during training
-        # per_device_eval_batch_size=16,   # batch size for evaluation
+        per_device_train_batch_size=8,  # batch size per device during training
         warmup_steps=500,                # number of warmup steps for learning rate scheduler
         weight_decay=0.01,               # strength of weight decay
         logging_dir='./logs',            # directory for storing logs
         logging_steps=100,              # log saving step.
-        # evaluation_strategy='steps', # evaluation strategy to adopt during training
+
+        per_device_eval_batch_size=8,   # batch size for evaluation
+        evaluation_strategy='steps',  # evaluation strategy to adopt during training
         # `no`: No evaluation during training.
         # `steps`: Evaluate every `eval_steps`.
         # `epoch`: Evaluate every end of epoch.
-        # eval_steps = 500,            # evaluation step.
+        eval_steps=500,            # evaluation step.
+        # dataloader_num_workers=4,
+        # label_smoothing_factor=0.5
+
+        # 제일 높은것만 저장한다고 한다.
+        # load_best_model_at_end=True,
+        # metric_for_best_model="accuracy"
+
     )
     trainer = Trainer(
         # the instantiated 🤗 Transformers model to be trained
         model=model,
         args=training_args,                  # training arguments, defined above
         train_dataset=RE_train_dataset,         # training dataset
-        # eval_dataset=RE_dev_dataset,             # evaluation dataset
-        # compute_metrics=compute_metrics         # define metrics function
+        eval_dataset=RE_dev_dataset,             # evaluation dataset
+        compute_metrics=compute_metrics         # define metrics function
     )
 
     # train model
@@ -81,6 +126,12 @@ def train():
 
 
 def main():
+    # seed고정
+    seed_everything(42)
+    # wandb설정
+    wandb.login()
+    wandb.init(project="bcai2-klue", name="kim-e4-b8_lr5e-5_notval",)
+
     train()
 
 
